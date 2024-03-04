02d42da61e6d366ebb782b9dbbdddc66b14ecd41
aqazasawax <130019068+aqazasawax@users.noreply.github.com>
2024-03-04 09:20:03 -0600

    Update v.js

--- v.js
+++ v.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      message: UPGRADES.tree1.11.desc
+      message: 'test'
     }
   }
+  computed: {
+    firstUpgDesc() {
+      if(player.class1 == 'warrior') {
+        return UPGRADES.tree1.11.desc
+      }
+      else {
+        return 'not a wa'
+      }
+    }
+  }
+  
 })
 
 app.mount('#app')
