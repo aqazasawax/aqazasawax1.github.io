a0d0da1ab9b53ddacf6856754e84720b504f44c9
aqazasawax <130019068+aqazasawax@users.noreply.github.com>
2024-03-04 09:03:06 -0600

    Create upgrades.js

---
+++ upgrades.js
@@ -0,0 +1,15 @@
+const UPGRADES = {
+        tree1: {
+                11: {
+                        desc: "Start your adventure as a warrior. Gain 1 point per second.",
+                        cost: new Decimal(1),
+                        costType: "points",
+                        unlocked() {
+
+                        },
+                        active() {
+                          
+                        },
+                },
+        },
+}
